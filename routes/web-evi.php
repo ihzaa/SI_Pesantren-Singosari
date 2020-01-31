bd5c4d29367f7ae26ac19db7a6eb61076fdf0307
Evi Febrion Rahayuningtyas <febrionrevi@gmail.com>
2020-01-31 14:12:20 +0700

    LAla

--- routes/web-evi.php
+++ routes/web-evi.php
@@ -2,6 +2,8 @@
 
 Route::prefix('pengajar')->middleware('CekStatusPengajar')->group(function () {
 
-    Route::get('/','PengajarController@index');
+    Route::get('/','PengajarController@index')->name('pengajaridx');
+    
+    // Route::get('/', 'adminController@adminidx')->name('adminidx');
 
 });
