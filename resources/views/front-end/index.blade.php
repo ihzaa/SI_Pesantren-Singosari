9003265ba0565a64b0ac897e1f39fb9994363092
Evi Febrion Rahayuningtyas <febrionrevi@gmail.com>
2020-02-13 11:34:19 +0700

    Fixing Front End Index

--- resources/views/front-end/index.blade.php
+++ resources/views/front-end/index.blade.php
@@ -430,12 +430,7 @@
                 <div class="col-lg-4 col-xs-12" style="color: aliceblue">
                     <br>
                     <h4 class="mt-lg-0 mt-sm-4">Lokasi</h4>
-<<<<<<< HEAD
                     <p><i class="fas fa-map-marker-alt mr-3"></i> Jl. Sempit Biru Desa Gunungrejo Kecamatan Singosari Kabupaten Malang Jawa Timur 65153</p>
-=======
-                    <p><i class="fas fa-map-marker-alt mr-3"></i> Jl. Sempit, Biru, Gunungrejo, Kec. Singosari, Malang,
-                        Jawa Timur 65153</p>
->>>>>>> d175f89a4ed9c6cb1ca244c458b067f126b65243
                     <h4 class="mt-lg-0 mt-sm-4">Telepon</h4>
                     <p class="mb-0"><i class="fa fa-phone mr-3"></i>081359069006</p>
                 </div>
